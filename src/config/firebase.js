21a3b29da9d390b90a3854278aa0b236099611c8
caalexandre7 <130807590+caalexandre@users.noreply.github.com>
2023-09-18 17:04:25 -0300

    v0.01

---
+++ src/config/firebase.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+//Aqui vocês devem colocar as suas credenciais
+const firebaseConfig = {
+  apiKey: "AIzaSyBNvwSsCADS068bGnc9aiOJMziKQvbqi2Y",
+  authDomain: "cauaifal523.firebaseapp.com",
+  projectId: "cauaifal523",
+  storageBucket: "cauaifal523.appspot.com",
+  messagingSenderId: "1071488950898",
+  appId: "1:1071488950898:web:211f63aa3116fb0f3898eb",
+  measurementId: "G-F0BWZL98NL"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+})
+
+export { app, auth }
